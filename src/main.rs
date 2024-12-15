345663e60ecbe80563b8896e60309edb2c42bf06
Knarkzel <knarkzel@gmail.com>
2024-12-15 15:14:34 +0100

    simple image resizer

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,33 @@
-use axum::{routing::post, Json, Router};
-use serde::{Deserialize, Serialize};
+use axum::{extract::Multipart, http::header, response::IntoResponse, routing::post, Router};
 use server::{error, Result};
+use std::io::Cursor;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // Create server
-    let app = Router::new().route("/users", post(create_user));
+    let app = Router::new().route("/resize", post(resize));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     Ok(axum::serve(listener, app).await?)
 }
 
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
+async fn resize(mut multipart: Multipart) -> Result<impl IntoResponse> {
+    if let Some(field) = multipart.next_field().await? {
+        // Load image
+        let data = field.bytes().await?;
+        let image = image::load_from_memory(&data)?;
 
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
-}
+        // Calculate new height maintaining aspect ratio
+        let new_width = 256;
+        let ratio = image.width() as f32 / new_width as f32;
+        let new_height = (image.height() as f32 / ratio) as u32;
+        let resized = image.resize(new_width, new_height, image::imageops::FilterType::Lanczos3);
 
-async fn create_user(Json(data): Json<CreateUser>) -> Result<Json<User>> {
-    // Create user if username is not admin
-    if data.username != "admin" {
-        Ok(Json(User {
-            id: 1337,
-            username: data.username,
-        }))
+        // Convert to bytes
+        let mut bytes = Vec::new();
+        let mut cursor = Cursor::new(&mut bytes);
+        resized.write_to(&mut cursor, image::ImageFormat::Jpeg)?;
+        Ok(([(header::CONTENT_TYPE, "image/jpeg")], bytes))
     } else {
-        Err(error!("Creating admin user is illegal"))?
+        Err(error!("No file uploaded"))?
     }
 }
